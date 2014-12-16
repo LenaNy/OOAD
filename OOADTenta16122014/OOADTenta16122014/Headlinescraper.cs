c0548fbab185361bddf8fa3dc167ba8d2e2a70f2
LenaNy <lenanygards@hotmail.se>
2014-12-16 09:40:01 +0100

    Headlinescraper
    HeadlineSystem
    Iscraper

---
+++ OOADTenta16122014/OOADTenta16122014/Headlinescraper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HtmlAgilityPack;
+
+namespace OOADTenta16122014
+{
+    public abstract class Headlinescraper : IScraper
+
+    {
+        public virtual String GetUrl { get; set; }
+        public virtual String GetXPath { get; set; }
+        public virtual string  GetSiteName { get; set; 
+    
+        protected HeadlineSystem mediator; }
+
+
+        public void Readnews()
+        {
+            var htmlWeb = new HtmlWeb();
+            string headline = String.Empty;
+            var doc = htmlWeb.Load(GetUrl);
+
+            var nodes = doc.DocumentNode.SelectNodes(GetXPath);
+
+            foreach (var title in nodes)
+            {
+                headline = title.InnerText;
+            }
+
+            this.mediator.FoundNews(GetSiteName, headline);
+
+
+        }
+    }
+
+
+
+}
---
+++ OOADTenta16122014/OOADTenta16122014/Iscraper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOADTenta16122014
+{
+    public interface IScraper
+    {
+        void Readnews();
+    }
+}
